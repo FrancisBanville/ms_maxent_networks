85d663321c0c5a98b90212625faf883651eead57
FrancisBanville <francis.banville@umontreal.ca>
2021-04-28 15:17:03 -0400

    :sparkles: predict adjacency matrix of maxent (first try)

--- main.jl
+++ main.jl
@@ -7,8 +7,10 @@ Pkg.instantiate()
 import CSV
 using DataFrames
 
+using LinearAlgebra
 using Plots
 using ProgressMeter
+using SparseArrays
 using Statistics
 using StatsBase
 using StatsPlots
@@ -18,6 +20,242 @@ using EcologicalNetworks
 using Mangal
 using RandomBooleanMatrices
 
+
+# Define custom functions
+
+"""
+predict_links(S::T, n::T, chain) where {T <: Int64}
+    S: number of species
+    n: number of simulations
+    chain: Monte-Carlo chain of the flexible links model fitted to empirical data
+Returns n values of predicted numbers of links
+"""
+function predict_links(S::T, n::T, chain) where {T <: Int64}
+    predicted_links = zeros(Int64, n)
+    p = get_params(chain[200:end,:,:])
+    for i in 1:n
+        # select parameter values randomly
+        p_i = rand(1:length(p.μ))
+        μ, ϕ = p.μ[p_i], p.ϕ[p_i]
+        # predict number of links 
+        predicted_links[i] = rand(BetaBinomial(S^2-(S-1), μ*exp(ϕ), (1-μ)*exp(ϕ))) + (S-1)
+    end
+    return predicted_links
+  end
+
+
+"""
+p_k(S::T, L::T, k::T) where {T <: Int64}
+    S: number of species
+    L: number of links
+    k: degree (number of species interacting with a given species)
+Returns the probability that a species has a degree k (derived from maximum entropy - analytical solution)
+"""
+function p_k(S::T, L::T, k::T) where {T <: Int64}
+    # mean degree constraint
+    kavg = 2*L/S 
+    # probability that a species has a degree of k
+    c = 1/(kavg - 1)
+    r = (kavg - 1)/kavg
+    p_k = c*(r^k)
+    return p_k
+end
+
+
+"""
+dd_maxent_prob(S::T, L::T) where {T <: Int64}
+    S: number of species
+    L: number of links
+Returns the degree distribution of maximum entropy (probability that a species has a degree k)
+"""
+function dd_maxent_prob(S::T, L::T) where {T <: Int64}
+    dd_maxent_prob = [p_k(S, L, k) for k in 1:S]
+    return dd_maxent_prob
+end
+  
+
+"""
+dd_maxent(S::T, L::T) where {T <: Int64}
+    S: number of species
+    L: number of links
+Returns the expected degree for all species (ordered from lowest to highest)
+"""
+function dd_maxent(S::T, L::T) where {T <: Int64}
+    # degree distribution of maximum entropy
+    dd_maxent_proba = dd_maxent_prob(S, L)
+    # cumulative number of species expected to have a degree of k or lower
+    dd_maxent_cum = cumsum(dd_maxent_proba) .* S
+    # number of species expected to have a degree of k
+    dd_maxent = zeros(Int64, S)
+    K = 1:S
+    for k in K
+      dd_maxent[k] = convert(Int64, round(dd_maxent_cum[k] - sum(dd_maxent)))
+    end
+    # expected degree for all species (ordered from lowest to highest)
+    dd_maxent = vcat(fill.(K, dd_maxent)...)
+    return dd_maxent
+end
+
+
+"""
+p_kin_kout(S::T, L::T, kin::T, kout::T) where {T <: Int64}
+    S: number of species
+    L: number of links
+    kin: in-degree (number of predators of a species)
+    Kout: out-degree (number of preys of a species)
+Returns the probability that a species has kin predators and kout preys (derived from maximum entropy - analytical solution)
+"""
+function p_kin_kout(S::T, L::T, kin::T, kout::T) where {T <: Int64}
+    # a species cannot have a total degree k = 0 or higher than the number of species
+    if (kin + kout == 0 || kin + kout > S)
+        p_kin_kout = 0
+    else
+        # mean degree constraint
+        kavg = 2L/S 
+        # probability that a species has an in-degree of kin and an out-degree of kout
+        k = kin + kout
+        c = 1/(kavg - 1)
+        r = (kavg - 1)/kavg
+        p_kin_kout = binomial(BigInt(k), BigInt(kin))*c*(r/2)^k
+    end
+    return p_kin_kout
+end
+  
+
+"""
+jdd_maxent_prob(S::T, L::T) where {T <: Int64}
+    S: number of species
+    L: number of links
+Returns the joint degree distribution of maximum entropy (probability that a species has an in-degree of kin and an out-degree of kout)
+"""
+function jdd_maxent_prob(S::T, L::T) where {T <: Int64}
+    jdd_maxent_prob = zeros(Float64, S+1, S+1)
+    # probability that a species has kin predators and kout preys for all values of kin and kout
+    for kin in 0:S, kout in 0:S
+      jdd_maxent_prob[kin+1, kout+1] = p_kin_kout(S, L, kin, kout)
+    end
+    return jdd_maxent_prob
+end
+
+
+"""
+indd_maxent(S::T, L::T) where {T <: Int64}
+    S: number of species
+    L: number of links
+Returns the expected in-degree for all species (ordered from lowest to highest)
+"""
+function indd_maxent(S::T, L::T) where {T <: Int64}
+    # joint degree distribution of maximum entropy
+    jdd_maxent_proba = jdd_maxent_prob(S, L)
+    # cumulative number of species expected to have an in-degree kin or lower
+    indd_maxent_cum = cumsum(sum.(eachcol(jdd_maxent_proba))) .* S
+    # number of species expected to have an in-degree kin for all possible values of kin
+    indd_maxent = zeros(Int64, S+1)
+    Kin = 0:S
+    for kin in Kin
+      indd_maxent[kin+1] = convert(Int64, round(indd_maxent_cum[kin+1] - sum(indd_maxent)))
+    end
+    # expected in-degree for all species (ordered from lowest to highest)
+    indd_maxent = vcat(fill.(Kin, indd_maxent)...)
+    return indd_maxent
+end
+
+
+"""
+matrix_rowcolsum(indd::T, outdd::T) where {T <: Vector{Int64}}
+    indd: in-degree distribution 
+    outdd: out-degree distributon 
+Returns an adjacency matrix constrained by the in-degree and out-degree distributions
+"""
+function matrix_rowcolsum(indd::T, outdd::T) where {T <: Vector{Int64}}
+    n = length(outdd)
+    A = zeros(Bool, n, n)
+    # row and column sums
+    row_remain = copy(outdd)
+    col_remain = copy(indd)
+    # assign interactions iteratively until no more can be assigned
+    while sum(row_remain) > 0
+        # identify a species pair that could interact
+        row_min = minimum(row_remain[row_remain .> 0])
+        col_max = maximum(col_remain[col_remain .> 0])
+        i = first(findall(row_remain .== row_min))
+        j = first(findall(col_remain .== col_max))
+        # assign the interaction
+        A[i, j] = 1
+        # adjust row and column sums
+        row_remain[i] = row_remain[i] - 1
+        col_remain[j] = col_remain[j] - 1
+    end
+    return A
+end
+
+
+"""
+svd_entropy(A::T) where {T <: Matrix{Bool}}
+    A: adjacency matrix
+Returns the entropy of the adjacency matrix of a deterministic network using the singular values from a Singualr Value Decomposition
+"""
+function svd_entropy(A::T) where {T <: Matrix{Bool}}
+    F = svd(A)
+    Λ = F.S[1:rank(A)]
+    λ = Λ ./ sum(Λ)
+    return -sum(λ .* log.(λ)) * 1 / log(length(λ))
+end
+
+
+"""
+adjacency_matrix_maxent(A::T) where {T <: Matrix{Bool}}
+    A: adjacency matrix
+    n: number of permutations
+Returns the adjacency matrix of maximum entropy constrained by row and column sums
+"""
+function adjacency_matrix_maxent(A::T, n::Int64) where {T <: Matrix{Bool}}
+    # matrix generator object
+    rmg = matrixrandomizer(A)
+    # adjacency matrix of maximum entropy identified so far
+    Amax = A
+    svd_entropy_max = svd_entropy(A)
+    for i in 1:n
+        # generate a new matrix with the same row and column sums
+        Anew = convert(Matrix{Bool}, rand(rmg))
+        # SVD-entropy of that new matrix
+        svd_entropy_new = svd_entropy(Anew)
+        # keep that matrix if it has a higer entropy than all matrices checked so far
+        if svd_entropy_new > svd_entropy_max
+            Amax = Anew
+            svd_entropy_max = svd_entropy_new
+        end
+    end
+    return Amax
+end
+
+
+"""
+predict_adjacency_matrix(S::T, L::T) where {T <: Int64}
+    S: number of species
+    L: number of links
+    n: number of permutations
+Returns the adjacency matrix of maximum entropy 
+"""
+function predict_adjacency_matrix(S::T, L::T, n::T) where {T <: Int64}
+    # degree distribution of maximum entropy
+    dd = dd_maxent(S, L)
+    # in-degree distribution of maximum entropy
+    indd = indd_maxent(S, L)
+    # out-degree distribution
+    outdd = dd .- indd
+    # making sure the in-degree and out-degree distributions have the same sum
+    m = length(dd)
+    outdd[m] = last(outdd) + (sum(indd) - sum(outdd))
+    # generate one matrix constrained by the in-degree and out-degree distributions
+    A = matrix_rowcolsum(indd, outdd)
+    # select the adjacency matrix with maximum SVD-entropy after n permutations
+    A = adjacency_matrix_maxent(A, n)
+    return A
+end
+
+
 # Load scripts
 include("src/01_import_mangal_metadata.jl")
 include("src/02_predict_networks.jl")
+
--- src/02_predict_networks.jl
+++ src/02_predict_networks.jl
@@ -1,25 +1,21 @@
-# Here we predict the number of links in food webs from their number of species (flexible links model)
-# We then predict their joint degree distributions from their number of species and number of links (MaxEnt)
-# We then predict their adjacency matrix from their joint degree distribution and select the one with maximum entropy
-
-## Read metadata of all food webs archived on mangal.io
+## Read metadata of all food webs archived on mangal.io (generated from 01_import_mangal_metadata.jl)
 
 mangal_foodwebs = DataFrame(CSV.File(joinpath("data", "mangal_foodwebs.csv")))
 
 
 ## Define flexible links model and infer model parameters
 
-# S: number of species in the network
-# R: number of flexible links in the network (links above the minimum)
+# S: number of species 
+# R: number of flexible links that are realized (links above the minimum)
 
 @model FL(S,R) = begin
-  N = length(S)
-  # Number of trials
+  # number of trials
+  N = length(S) 
   F = S.*S .- (S.-1)
-  # Parameters (priors)
+  # parameters (priors)
   ϕ ~ Normal(3.0, 0.5)
   μ ~ Beta(3.0, 7.0)
-  # Flexible links model
+  # flexible links model
   for i in 1:N
     R[i] ~ BetaBinomial(F[i], μ*exp(ϕ), (1-μ)*exp(ϕ))
   end
@@ -27,9 +23,13 @@ mangal_foodwebs = DataFrame(CSV.File(joinpath("data", "mangal_foodwebs.csv")))
 end
 
 # Observations
-S = vec(mangal_foodwebs[:,:S])
-L = vec(mangal_foodwebs[:,:L])
-R = L .- (S.-1)
+
+# number of species
+S = vec(mangal_foodwebs[:,:S]) 
+# number of links
+L = vec(mangal_foodwebs[:,:L]) 
+# number of flexible links that are realized
+R = L .- (S.-1) 
 
 # Hamiltonian Monte Carlo sampler with static trajectory
 chain = sample(FL(S,R), HMC(0.01,10), 3000)
@@ -40,177 +40,50 @@ plot(chain[200:end,:,:])
 
 ## Simulate counterfactuals
 
-# Predict the number of links for a given number of species
-function predict_links(chain, S::Int64, nsim::Int64)
-  p = get_params(chain[200:end,:,:])
-  i = rand(1:length(p.μ))
-  μ, ϕ = p.μ[i], p.ϕ[i]
-  return rand(BetaBinomial(S^2-(S-1), μ*exp(ϕ), (1-μ)*exp(ϕ)), nsim) .+ (S-1)
-end
-
 # Simulate the number of links for a range of species richness
-sp = collect(5:1000) # numbers of species
-nsp = length(sp)
-nsim = 1000 # number of simulations
 
-predicted_links = zeros(Int64, nsim, length(sp))
+# numbers of species
+sp = collect(5:1000) 
+nsp = length(sp)
+# number of simulations
+n = 1000 
+
+predicted_links = zeros(Int64, n, length(sp))
 
 p = Progress(length(sp))
 Threads.@threads for s in sp
     i = findall(sp .== s)
-    predicted_links[:, i] = predict_links(chain, s, nsim)
+    predicted_links[:, i] = predict_links(s, n, chain)
     next!(p)
 end
 
 
+## Predict the adjacency matrix of maximum entropy
 
-## Predict the degree distribution of maximum entropy (approximation of the analytical solution)
+# Example
 
-function p_k(S::Int64, L::Int64, k::Int64)
-  # S: Number of species
-  # L: Number of links
-  # k: Species degree
-
-  # Mean degree constraint
-  kavg = 2*L/S 
-
-  # Degree distribution of maximum entropy
-  c = 1/(kavg - 1)
-  r = (kavg - 1)/kavg
-  p_k = c*(r^k)
-
-  # Return the probability that a species has k links
-  return p_k
-end
-
-
-function dd_maxent_prob(S::Int64, L::Int64)
-  # S: Number of species
-  # L: Number of interactions
-
-  dd_maxent_prob = [p_k(S, L, k) for k in 1:S]
-
-  # Return the degree distribution (probabilistic)
-  return dd_maxent_prob
-end
-
-
-function dd_maxent(S::Int64, L::Int64)
-  # S: Number of species
-  # L: Number of interactions
-
-  # Predict the degree distribution (probabilistic)
-  dd_maxent_proba = dd_maxent_prob(S, L)
-
-  # Cumulative number of species expected to have degree k or lower
-  dd_maxent_cum = cumsum(dd_maxent_proba) .* S
-
-  # Number of species expected to have degree k
-  dd_maxent = zeros(Int64, S)
-
-  K = 1:S
-
-  for k in K
-    dd_maxent[k] = convert(Int64, round(dd_maxent_cum[k] - sum(dd_maxent)))
-  end
-  
-  # Return the expected degree for all species (ordered)
-  dd_maxent = vcat(fill.(K, dd_maxent)...)
-
-  return dd_maxent
-
-end
-
-
-# Example 1
-S = 110
-L = predict_links(chain, S, nsim)
+# number of species
+S = 50
+# predicted number of links (take median)
+L = predict_links(S, n, chain)
 L = convert(Int64, round(median(L)))
-ddist = dd_maxent(S, L)
-sum(ddist)
-L*2
-
-
-## Predict the joint degree distribution of maximum entropy (analytical approximation)
-
-function p_kin_kout(S::Int64, L::Int64, kin::Int64, kout::Int64)
-  # S: Number of species
-  # L: Number of links
-  # kin: Species in-degree
-  # Kout: Species out-degree
-
-  # A species cannot have a degree of k = 0
-  if (kin + kout == 0 || kin + kout > S)
-    p_kin_kout = 0
-  else
-
-  # Mean degree constraint
-  kavg = 2L/S 
-
-  # Joint degree distribution of maximum entropy
-  k = kin + kout
-  c = 1/(kavg - 1)
-  r = (kavg - 1)/kavg
-  p_kin_kout = binomial(BigInt(k), BigInt(kin))*c*(r/2)^k
- 
-   # Return the probability that a species has a kin predators and kout preys
-   return p_kin_kout
-
-  end
- end
-
-
- function jdd_maxent_prob(S::Int64, L::Int64)
-  # S: Number of species
-  # L: Number of interactions
-
-  jdd_maxent_prob = zeros(Float64, S+1, S+1)
-  
-  for kin in 0:S, kout in 0:S
-    jdd_maxent_prob[kin+1, kout+1] = p_kin_kout(S, L, kin, kout)
-  end
-  
-  # Return the joint degree distribution (probabilistic)
-  return jdd_maxent_prob
-end
-
-
-
-function jdd_maxent(S::Int64, L::Int64)
-  # S: Number of species
-  # L: Number of interactions
-
-  # Predict the degree distribution (probabilistic)
-  jdd_maxent_proba = jdd_maxent_prob(S, L)
-
-  # Cumulative number of species expected to have an in-degree kin or lower
-  jdd_maxent_cum = cumsum(sum.(eachcol(jdd_maxent_proba))) .* S
-
-  # Number of species expected to have an in-degree k
-  jdd_maxent = zeros(Int64, S+1)
-
-  Kin = 0:S
-
-  for kin in Kin
-    jdd_maxent[kin+1] = convert(Int64, round(jdd_maxent_cum[kin+1] - sum(jdd_maxent)))
-  end
-  
-  # Return the expected in-degree for all species (ordered)
-  jdd_maxent = vcat(fill.(Kin, jdd_maxent)...)
-
-  return jdd_maxent
-
-end
-
-
-# Example 2
-S = 225
-L = predict_links(chain, S, nsim)
-L = convert(Int64, round(median(L)))
-ddist = dd_maxent(S, L)
-jddin = jdd_maxent(S, L)
-jddout = ddist .- jddin
-
-
+# degree distribution of maximum entropy (probabilities)
+dd_maxent_prob(S, L)
+# degree distribution of maximum entropy (counts)
+dd = dd_maxent(S, L)
+# joint-degree distribution of maximum entropy (probabilities)
+jdd_maxent_prob(S, L)
+# in-degree distribution of maximum entropy (counts)
+indd = indd_maxent(S, L)
+# out-degree distribution of maximum entropy (non-adjusted counts)
+outdd = dd.- indd
+m = length(dd)
+outdd[m] = last(outdd) + (sum(indd) - sum(outdd))
+# first adjacency matrix constrained by in-degree and out-degree distributions
+A = matrix_rowcolsum(indd, outdd)
+# adjacency matrix of maximum entropy
+A = adjacency_matrix_maxent(A, 50)
+# predict adjacency matrix of maximum entropy in one command
+B = predict_adjacency_matrix(S, L, 50)
 
 
