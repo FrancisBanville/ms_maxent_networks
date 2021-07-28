31bddb79066843548824cd0296a227c9daf1b915
Francis Banville <francis.banville@umontreal.ca>
2021-07-28 16:19:30 -0400

    :sparkles: make figure on degree distribution comparison

--- src/03_make_figures.jl
+++ src/03_make_figures.jl
@@ -149,18 +149,143 @@ plot(plotA, plotB)
 savefig(joinpath("figures","maxent_degree_distributions"))
 
 
+## Figure: MaxEnt and empirical degree distributions
+
+# Get Mangal food webs with given numbers of species (median and 97% PI)
+species985 = 81 # no network with 85 species (but some with 81 and 89 species)
+
+Ns_mangal500 = mangal_foodwebs[mangal_foodwebs.S .== species500, :]
+Ns_mangal015 = mangal_foodwebs[mangal_foodwebs.S .== species015, :]
+Ns_mangal985 = mangal_foodwebs[mangal_foodwebs.S .== species985, :] 
+
+# Get food webs with minimum and maximum numbers of links (from food webs with median and 97% PI of species richness) (arbitrary criteria to select networks that will be plotted)
+N_mangal500min = Ns_mangal500[Ns_mangal500.L .== minimum(Ns_mangal500.L), :][1,:]
+N_mangal500max = Ns_mangal500[Ns_mangal500.L .== maximum(Ns_mangal500.L), :]
+N_mangal015min = Ns_mangal015[Ns_mangal015.L .== minimum(Ns_mangal015.L), :][2,:] # 2 food webs with 7 species and 8 links (get the one with predatory links)
+N_mangal015max = Ns_mangal015[Ns_mangal015.L .== maximum(Ns_mangal015.L), :]
+N_mangal985min = Ns_mangal985[Ns_mangal985.L .== minimum(Ns_mangal985.L), :]
+N_mangal985max = Ns_mangal985[Ns_mangal985.L .== maximum(Ns_mangal985.L), :]
+
+"""
+get_ranked_dd(N::DataFrameRow)
+    N: Data frame row of Mangal metadata
+Returns the ranked degree distribution of the input network
+"""
+function get_ranked_dd(N::DataFrameRow)
+    # Convert to UnipartiteNetwork
+    N = convert(UnipartiteNetwork, network(N.id))
+    # Get sorted degree distribution 
+    dd = sort(collect(values(degree(N))), rev=true)
+    return dd
+end
+
+# Get degree distribution for all selcted food webs
+dd_mangal500min = get_ranked_dd(N_mangal500min)
+dd_mangal500max = get_ranked_dd(N_mangal500max)
+dd_mangal015min = get_ranked_dd(N_mangal015min)
+dd_mangal015max = get_ranked_dd(N_mangal015max)
+dd_mangal985min = get_ranked_dd(N_mangal985min)
 
 
-## Read metadata of all food webs archived on mangal.io (generated from 01_import_mangal_metadata.jl)
-mangal_foodwebs = DataFrame(CSV.File(joinpath("data", "mangal_foodwebs.csv")))
+# Get simulated networks with simulated numbers of links (median of flexible links model)
+Ns_maxent_fl = load(joinpath("data", "sim", "predicted_networks.jld"))["data"]
 
-## Read predicted networks 
+# Get simulated networks with same numbers of species as the food webs previously selected
+S_maxent_fl = richness.(Ns_maxent_fl)
 
-# Simulated networks with simulated numbers of links
-predicted_networks = load(joinpath("data", "sim", "predicted_networks.jld"))["data"]
+N_maxent_fl500 = Ns_maxent_fl[S_maxent_fl .== species500][1]
+N_maxent_fl015 = Ns_maxent_fl[S_maxent_fl .== species015][1]
+N_maxent_fl985 = Ns_maxent_fl[S_maxent_fl .== species985][1]
 
-# Simulated networks with empirical numbers of links and species
-predicted_networks_empL = load(joinpath("data", "sim", "predicted_networks_empL.jld"))["data"]
+"""
+get_ranked_dd(N::UnipartiteNetwork)
+    N: Unipartite Network
+Returns the ranked degree distribution of the input network
+"""
+function get_ranked_dd(N::UnipartiteNetwork)
+  # Get sorted degree distribution 
+  dd = sort(collect(values(degree(N))), rev=true)
+  return dd
+end
+
+# Get degree distributions of all selected simulated networks
+dd_maxent_fl500 = get_ranked_dd(N_maxent_fl500)
+dd_maxent_fl015 = get_ranked_dd(N_maxent_fl015)
+dd_maxent_fl985 = get_ranked_dd(N_maxent_fl985)
+
+
+# Get simulated networks with the same numbers of links as food webs on Mangal
+Ns_maxent_empL = load(joinpath("data", "sim", "predicted_networks_empL.jld"))["data"]
+
+"""
+get_network_empL(S::Int64, fun::String) 
+    S: Species richness
+    fun: Either "min" or "max"
+Returns the simulated network of S species with the lowest (fun = "min") or higest (fun = "max") numbers of links
+"""
+function get_network_empL(S::Int64, fun::String) 
+  # Get simulated networks with S species
+  Ns_S = Ns_maxent_empL[richness.(Ns_maxent_empL) .== S]
+  
+  # Get the network with the lowest (min) or highest (max) numbers of links
+  if fun == "min"
+    N = Ns_S[links.(Ns_S) .== minimum(links.(Ns_S))][1]
+  elseif fun == "max"
+    N = Ns_S[links.(Ns_S) .== maximum(links.(Ns_S))][1]
+  else 
+    message("The function must be min or max")
+  end
+  return N
+end
+
+# Get simulated networks with same numbers of species and links as previously selected food webs
+N_maxent_empL500min = get_network_empL(species500, "min")
+N_maxent_empL500max = get_network_empL(species500, "max")
+N_maxent_empL015min = get_network_empL(species015, "min")
+N_maxent_empL015max = get_network_empL(species015, "max")
+N_maxent_empL985min = get_network_empL(species985, "min")
+
+# Get degree distributions of all selected simulated networks
+dd_maxent_empL500min = get_ranked_dd(N_maxent_empL500min)
+dd_maxent_empL500max = get_ranked_dd(N_maxent_empL500max)
+dd_maxent_empL015min = get_ranked_dd(N_maxent_empL015min)
+dd_maxent_empL015max = get_ranked_dd(N_maxent_empL015max)
+dd_maxent_empL985min = get_ranked_dd(N_maxent_empL985min)
+
+
+"""
+plot_dd(mangalmin::T, mangalmax::T, empLmin::T, empLmax::T, fl::T) where {T <: Vector{Int64}}
+    mangalmin: Degree distribution of Mangal food web of S species with lowest number of links
+    mangalmax: Degree distribution of Mangal food web of S species and highest number of links
+    empLmin: Degree distribution of simulated food web of S species and lowest number of links (same number of links as Mangal food webs)
+    empLmax: Degree distribution of simulated food web of S species and highest number of links (same number of links as Mangal food webs)
+    fl: Degree distribution of simulated food web of S and number of links given by the median of the flexible links model
+Returns a plot of the degree distributions of Mangal and empirical food webs with S species
+"""
+function plot_dd(mangalmin::T, mangalmax::T, empLmin::T, empLmax::T, fl::T) where {T <: Vector{Int64}}
+    plot(mangalmin, color=:black, alpha=0.7, linewidth=2, linestyle=:dot, label="Mangal", 
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      xlab="Rank", ylab="Degree k")
+  plot!(mangalmax, color=:darkblue, alpha=0.7, linewidth=2, linestyle=:dot, label="")
+  plot!(empLmin, color=:black, alpha=0.7, linewidth=2, linestyle=:solid, label="MaxEnt")
+  plot!(empLmax, color=:darkblue, alpha=0.7, linewidth=2, linestyle=:solid, label="")
+  plot!(fl, color=:darkred, alpha=0.7, linewidth=2, linestyle=:solid, label="")
+end
+
+# Plot the degree distributions of Mangal and empirical food webs with median and 97% PI numbers of species
+plotA = plot_dd(dd_mangal015min, dd_mangal015max, dd_maxent_empL015min, dd_maxent_empL015max, dd_maxent_fl015)
+plotB = plot_dd(dd_mangal500min, dd_mangal500max, dd_maxent_empL500min, dd_maxent_empL500max, dd_maxent_fl500)
+plotC = plot_dd(dd_mangal985min, dd_mangal985min, dd_maxent_empL985min, dd_maxent_empL985min, dd_maxent_fl985)
+
+plot(plotA, plotB, plotC, 
+    layout=(1,3),
+    title = ["S=$(species015) species" "S=$(species500) species" "S=$(species985) species"],
+    titleloc=:right, titlefont=fonts)
+
+savefig(joinpath("figures","mangal_maxent_degree_distributions"))
+
 
 
 ## Compute measures of food webs of maximum entropy (simulated numbers of links)
