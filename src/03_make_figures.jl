2ed23c984e164f2806ccc354d1a253a34a39b41e
Francis Banville <francis.banville@umontreal.ca>
2021-07-29 16:31:05 -0400

    :sparkles: make figures 4 and 6

--- src/03_make_figures.jl
+++ src/03_make_figures.jl
@@ -101,7 +101,7 @@ kavg985 = get_avgk_dist(species985)
 plotA = density(kavg015, linewidth=2, label="$(species015) species",
       framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
       guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
-      foreground_color_legend=nothing, background_color_legend=:white)
+      foreground_color_legend=nothing, background_color_legend=:white, legendfont=fonts)
 density!(kavg500, linewidth=2, label="$(species500) species")
 density!(kavg985, linewidth=2, label="$(species985) species")
 xaxis!(xlabel="Mean degree (links/species)", xlims=(0,maximum(kavg985)))
@@ -120,13 +120,13 @@ function dd_maxent_prob(S::Int64, q::Float64)
 end
 
 # Get the degree distribution of MaxEnt (analytical solution) for a network with a median number of species and different numbers of links
-dd015 = dd_maxent_prob(species50, 0.015)
-dd055 = dd_maxent_prob(species50, 0.055)
-dd165 = dd_maxent_prob(species50, 0.165)
-dd500 = dd_maxent_prob(species50, 0.5)
-dd835 = dd_maxent_prob(species50, 0.835)
-dd945 = dd_maxent_prob(species50, 0.945)
-dd985 = dd_maxent_prob(species50, 0.985)
+dd015 = dd_maxent_prob(species500, 0.015)
+dd055 = dd_maxent_prob(species500, 0.055)
+dd165 = dd_maxent_prob(species500, 0.165)
+dd500 = dd_maxent_prob(species500, 0.5)
+dd835 = dd_maxent_prob(species500, 0.835)
+dd945 = dd_maxent_prob(species500, 0.945)
+dd985 = dd_maxent_prob(species500, 0.985)
 
 # Plot different degree distributions of MaxEnt (analytical solution) for a network with a median number of species
 a = [1,2,5,10,15,20,25] # specified x-ticks 
@@ -134,17 +134,19 @@ a = [1,2,5,10,15,20,25] # specified x-ticks
 plotB = plot(dd015, color=:black, alpha=0.7, linewidth=2, linestyle=:dot, label="97% PI", # 97% PI
     framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
     guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
-    foreground_color_legend=nothing, background_color_legend=:white)
+    foreground_color_legend=nothing, background_color_legend=:white, legendfont=fonts)
 plot!(dd055, color=:black, alpha=0.7, linestyle=:dash, label="89% PI") # 89% PI
 plot!(dd165, color=:black, alpha=0.7, linestyle=:solid, label="67% PI") # 67% PI
 plot!(dd835, color=:grey, alpha=0.7, linestyle=:solid, label="") # 67% PI
 plot!(dd945, color=:grey, alpha=0.7, linestyle=:dash, label="") # 89% PI
-plot!(dd_985, color=:grey, alpha=0.7, linestyle=:dot, label="") # 97% PI
+plot!(dd985, color=:grey, alpha=0.7, linestyle=:dot, label="") # 97% PI
 plot!(dd500, color=:darkblue, linewidth=4, label="median")
-xaxis!(:log, xlabel="Degree k", xticks=(a, a), xlims=(1,species50))
+xaxis!(:log, xlabel="Degree k", xticks=(a, a), xlims=(1,species500))
 yaxis!(ylabel="p(k)")
 
-plot(plotA, plotB)
+plot(plotA, plotB,
+    title = ["(a)" "(b)"],
+    titleloc=:right, titlefont=fonts)
 
 savefig(joinpath("figures","maxent_degree_distributions"))
 
@@ -266,7 +268,7 @@ function plot_dd(mangalmin::T, mangalmax::T, empLmin::T, empLmax::T, fl::T) wher
     plot(mangalmin, color=:black, alpha=0.7, linewidth=2, linestyle=:dot, label="Mangal", 
       framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
       guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
-      foreground_color_legend=nothing, background_color_legend=:white,
+      foreground_color_legend=nothing, background_color_legend=:white, legendfont=fonts,
       xlab="Rank", ylab="Degree k")
   plot!(mangalmax, color=:darkblue, alpha=0.7, linewidth=2, linestyle=:dot, label="")
   plot!(empLmin, color=:black, alpha=0.7, linewidth=2, linestyle=:solid, label="MaxEnt")
@@ -281,110 +283,180 @@ plotC = plot_dd(dd_mangal985min, dd_mangal985min, dd_maxent_empL985min, dd_maxen
 
 plot(plotA, plotB, plotC, 
     layout=(1,3),
-    title = ["S=$(species015) species" "S=$(species500) species" "S=$(species985) species"],
+    title = ["(a) S=$(species015) species" "(b) S=$(species500) species" "(c) S=$(species985) species"],
     titleloc=:right, titlefont=fonts)
 
 savefig(joinpath("figures","mangal_maxent_degree_distributions"))
 
 
 
-## Compute measures of food webs of maximum entropy (simulated numbers of links)
+## Figures: Measures of empirical and maximum entropy food webs 
 
-# richness 
-predicted_networks_S = richness.(predicted_networks)
-# nestedness (spectral radius of the adjacency matrix)
-predicted_networks_nestedness = ρ.(predicted_networks)
-# maximum and average trophic levels
-predicted_networks_tls = []
-working = []
-for i in 1:length(predicted_networks)
-  try
-  push!(predicted_networks_tls, values(trophic_level(predicted_networks[i])))
-  push!(working, i)
-  catch
-  println("could not estimate trophic levels in network $(i)")
-  end
-end
-
-trophic_level(transpose(predicted_networks))
-predicted_networks_tl_max = maximum.(predicted_networks_tls)
-predicted_networks_tl_avg = sum.(predicted_networks_tls)./length.(predicted_networks_tls)
-
-
-## Compute measures of food webs of maximum entropy (empirical numbers of links)
-
-# richness 
-predicted_networks_empL_S = richness.(predicted_networks_empL)
-# nestedness (spectral radius of the adjacency matrix)
-predicted_networks_empL_nestedness = ρ.(predicted_networks_empL)
-# maximum and average trophic levels
-predicted_networks_empL_tls = []
-working_empL = []
-for i in 1:length(predicted_networks_empL)
-  try
-  push!(predicted_networks_empL_tls, values(trophic_level(predicted_networks_empL[i])))
-  push!(working_empL, i)
-  catch
-  println("could not estimate trophic levels in network $(i)")
-  end
-end
-
-predicted_networks_empL_tl_max = maximum.(predicted_networks_empL_tls)
-predicted_networks_empL_tl_avg = sum.(predicted_networks_empL_tls)./length.(predicted_networks_empL_tls)
-
-
-## Measures of food webs archived on mangal.io
-
-# Read food webs and convert them to unipartie networks 
+# Convert food webs archieved on Mangal to UnipartiteNetworks
 Ns_mangal = network.(mangal_foodwebs.id)
 Ns_mangal = convert.(UnipartiteNetwork, Ns_mangal)
 
-# Compute food-web measures 
-# richness 
-Ns_mangal_S = richness.(Ns_mangal)
-# nestedness (spectral radius of the adjacency matrix)
-Ns_mangal_nestedness = ρ.(Ns_mangal)
-# maximum and average trophic levels
-Ns_mangal_tls = values.(trophic_level.(Ns_mangal))
-Ns_mangal_tl_max = maximum.(Ns_mangal_tls)
-Ns_mangal_tl_avg = sum.(Ns_mangal_tls)./length.(Ns_mangal_tls)
+# Measure species richness
+S_mangal = richness.(Ns_mangal)
+S_maxent_empL = richness.(Ns_maxent_empL)
+S_maxent_fl = richness.(Ns_maxent_fl)
 
-Ns_mangal_nestedness2 = zeros(length(Sp))
-Ns_mangal_tl_max2 = zeros(length(Sp))
-Ns_mangal_tl_avg = zeros(length(Sp))
+# Measure nestedness (spectral radius of the adjacency matrix)
+nestedness_mangal = ρ.(Ns_mangal)
+nestedness_maxent_empL = ρ.(Ns_maxent_empL)
+nestedness_maxent_fl = ρ.(Ns_maxent_fl)
 
-for s in Sp, i in 1:length(Sp)
-  j = findall(Ns_mangal_S .== s)
-  Ns_mangal_nestedness2[i] = median(Ns_mangal_nestedness[j])
-end 
+# Measure maximum trophic levels
+tl_mangal = values.(trophic_level.(Ns_mangal))
+tl_max_mangal = maximum.(tl_mangal)
+
+tl_maxent_empL = values.(trophic_level.(Ns_maxent_empL))
+tl_max_maxent_empL = maximum.(tl_maxent_empL)
+
+tl_maxent_fl = values.(trophic_level.(Ns_maxent_fl))
+tl_max_maxent_fl = maximum.(tl_maxent_fl)
+
+# Measure network diameter (shortest distance between the two most distant nodes in the network)
+shortest_paths_mangal = shortest_path.(Ns_mangal)
+diameter_mangal = maximum.(shortest_paths_mangal)
+
+shortest_paths_maxent_empL = shortest_path.(Ns_maxent_empL)
+diameter_maxent_empL = maximum.(shortest_paths_maxent_empL)
+
+shortest_paths_maxent_fl = shortest_path.(Ns_maxent_fl)
+diameter_maxent_fl = maximum.(shortest_paths_maxent_fl)
+
+# Measure SVD-entropy
+"""
+svd_entropy(N::T) where {T <: UnipartiteNetwork}
+    N: unipartite network
+Returns the entropy of the adjacency matrix of a deterministic network using the singular values from a Singualar Value Decomposition
+"""
+function svd_entropy(N::T) where {T <: UnipartiteNetwork}
+    A = convert(Matrix, N.edges)
+    F = svd(A)
+    Λ = F.S[1:rank(A)]
+    λ = Λ ./ sum(Λ)
+    return -sum(λ .* log.(λ)) * 1 / log(length(λ))
+end
+
+entropy_mangal = svd_entropy.(Ns_mangal)
+entropy_maxent_empL = svd_entropy.(Ns_maxent_empL)
+entropy_maxent_fl = svd_entropy.(Ns_maxent_fl)
+
+# Make vector of measures of MaxEnt food webs (nb of links given by the flexible links model) the same length as the other vectors
+n1 = length(Ns_mangal) 
+n2 = length(Ns_maxent_fl)
+i = [findall(in(mangal_foodwebs.S[i]).(S_maxent_fl)) for i in 1:n1]
+i[n1-1] = [n2] # The second to last entry has no value. We'll change it manually.
+i = reduce(vcat, i)
+
+nestedness_maxent_fl_long = [nestedness_maxent_fl[j] for j in i]
+tl_max_maxent_fl_long = [tl_max_maxent_fl[j] for j in i]
+diameter_maxent_fl_long = [diameter_maxent_fl[j] for j in i]
+entropy_maxent_fl_long = [entropy_maxent_fl[j] for j in i]
+
+# Plot predicted as a function of empirical measures
+plotA = scatter(nestedness_mangal, nestedness_maxent_empL, alpha=0.3, markersize=3, label="Empirical L",
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      legend=:bottomright, legendfont=fonts,
+      xlabel="Nestedness (empirical webs)",
+      ylabel="Nestedness (MaxEnt webs)")
+scatter!(nestedness_mangal, nestedness_maxent_fl_long, alpha=0.3, markersize=3, label="Median L")
+plot!(LinRange(0.4, 0.9, 100), LinRange(0.4, 0.9, 100), lab="", color="grey", linestyle=:dot)
+
+plotB = scatter(tl_max_mangal, tl_max_maxent_empL, alpha=0.3, markersize=3, label="Empirical L",
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      legend=:bottomright, legendfont=fonts,
+      ticks=[2,4,6,8,10,12],
+      xlabel="Maximum trophic level (empirical webs)",
+      ylabel="Maximum trophic level (MaxEnt webs)")
+scatter!(tl_max_mangal, tl_max_maxent_fl_long, alpha=0.3, markersize=3, label="Median L")
+plot!(LinRange(2, 12, 100), LinRange(2, 12, 100), lab="", color="grey", linestyle=:dot)
+
+plotC = scatter(diameter_mangal, diameter_maxent_empL, alpha=0.3, markersize=3, label="Empirical L",
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      legend=:bottomright, legendfont=fonts,
+      ticks=[1,3,5,7,9,11,13],
+      xlabel="Diameter (empirical webs)",
+      ylabel="Diameter (MaxEnt webs)")
+scatter!(diameter_mangal .+ 0.1, diameter_maxent_fl_long, alpha=0.3, markersize=3, label="Median L")
+plot!(LinRange(1, 13, 100), LinRange(1, 13, 100), lab="", color="grey", linestyle=:dot)
+
+plotD = scatter(entropy_mangal, entropy_maxent_empL, alpha=0.3, markersize=3, label="Empirical L",
+framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+foreground_color_legend=nothing, background_color_legend=:white,
+legend=:bottomright, legendfont=fonts,
+xlabel="SVD-entropy (empirical webs)",
+ylabel="SVD-entropy (MaxEnt webs)")
+scatter!(entropy_mangal, entropy_maxent_fl_long, alpha=0.3, markersize=3, label="Median L")
+plot!(LinRange(0.77, 1, 100), LinRange(0.77, 1, 100), lab="", color="grey", linestyle=:dot)
+
+plot(plotA, plotB, plotC, plotD, 
+     title = ["(a)" "(b)" "(c)" "(d)"],
+     titleloc=:right, titlefont=fonts)
+
+savefig(joinpath("figures", "measures_mangal_maxent.png"))
 
 
-## Figures
+# Plot measures as a function of species richness
+a = [5,10,20,50,100,400] # specified x-ticks 
 
-# Nestedness (MaxEnt and Mangal food webs)
-plotA = scatter(Ns_mangal_nestedness, predicted_networks_empL_nestedness, alpha=0.5, lab="", framestyle=:box, guidefont=fonts, xtickfont=fonts, ytickfont=fonts)
-plot!(LinRange(0.4, 0.9, 100), LinRange(0.4, 0.9, 100), lab="", color="grey")
-xaxis!("Empirical food webs")
-yaxis!("MaxEnt food webs (empirical number of links")
+plotA = scatter(S_mangal, nestedness_maxent_empL, alpha=0.3, label="Empirical L", smooth=true, markersize=3, linestyle=:dot, linealpha=1,
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      legend=:topright, legendfont=fonts,
+      xlabel="Species richness",
+      ylabel="Nestedness")
+scatter!(S_mangal, nestedness_maxent_fl_long, alpha=0.3, label="Median L", smooth=true, markersize=3, linestyle=:dot, linealpha=1)
+scatter!(S_mangal, nestedness_mangal, alpha=0.3, label="Mangal", smooth=true, markersize=3, linestyle=:dot, linealpha=1)
+xaxis!(:log, xticks=(a,a))
 
-plot(plotA, plotB, layout=(1,2), legend=false,
-    title=["flexible links" "empirical links"], titleloc=:right, titlefont=font(6))
+plotB = scatter(S_mangal, tl_max_maxent_empL, alpha=0.3, label="Empirical L", smooth=true, markersize=3, linestyle=:dot, linealpha=1,
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      legend=:topright, legendfont=fonts,
+      xlabel="Species richness",
+      ylabel="Maximum trophic level",
+      yticks=[2,4,6,8,10,12])
+scatter!(S_mangal, tl_max_maxent_fl_long, alpha=0.3, label="Median L", smooth=true, markersize=3, linestyle=:dot, linealpha=1)
+scatter!(S_mangal, tl_max_mangal, alpha=0.3, label="Mangal", smooth=true,markersize=3, linestyle=:dot, linealpha=1)
+xaxis!(:log, xticks=(a,a))
 
-savefig(joinpath("figures", "nestedness.png"))
+plotC = scatter(S_mangal, diameter_maxent_empL, alpha=0.3, label="Empirical L", smooth=true, markersize=3, linestyle=:dot, linealpha=1,
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      legend=:topright, legendfont=fonts, 
+      xlabel="Species richness",
+      ylabel="Diameter",
+      yticks=[1,3,5,7,9,11,13])
+scatter!(S_mangal, diameter_maxent_fl_long, alpha=0.3, label="Median L", smooth=true, markersize=3, linestyle=:dot, linealpha=1)
+scatter!(S_mangal, diameter_mangal, alpha=0.3, label="Mangal", smooth=true, markersize=3, linestyle=:dot, linealpha=1)
+xaxis!(:log, xticks=(a,a))
 
-# Maximum trophic level (MaxEnt and Mangal food webs)
-scatter(Ns_mangal_tl_max[working], predicted_networks_tl_max, alpha=0.5, lab="",
-title="Maximum trophic level", framestyle=:box, guidefont=fonts, xtickfont=fonts, ytickfont=fonts)
-plot!(LinRange(2, 13, 100), LinRange(2, 13, 100), lab="", color="grey")
-xaxis!("Empirical food webs")
-yaxis!("MaxEnt food webs")
-savefig(joinpath("figures", "maximum_trophic_level.png"))
+plotD = scatter(S_mangal, entropy_maxent_empL, alpha=0.3, label="Empirical L", smooth=true, markersize=3, linestyle=:dot, linealpha=1,
+      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
+      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
+      foreground_color_legend=nothing, background_color_legend=:white,
+      legend=:bottomright, legendfont=fonts, 
+      xlabel="Species richness",
+      ylabel="SVD-entropy")
+scatter!(S_mangal, entropy_maxent_fl_long, alpha=0.3, label="Median L", smooth=true, markersize=3, linestyle=:dot, linealpha=1)
+scatter!(S_mangal, entropy_mangal, alpha=0.3, label="Mangal", smooth=true, markersize=3, linestyle=:dot, linealpha=1)
+xaxis!(:log, xticks=(a,a))
 
-# Average trophic level (MaxEnt and Mangal food webs)
-scatter(Ns_mangal_tl_avg[working], predicted_networks_tl_avg, alpha=0.5, lab="",
-title="Average trophic level", framestyle=:box, guidefont=fonts, xtickfont=fonts, ytickfont=fonts)
-plot!(LinRange(1, 7, 100), LinRange(1, 7, 100), lab="", color="grey")
-xaxis!("Empirical food webs")
-yaxis!("MaxEnt food webs")
-savefig(joinpath("figures", "average_trophic_level.png"))
+plot(plotA, plotB, plotC, plotD, 
+     title = ["(a)" "(b)" "(c)" "(d)"],
+     titleloc=:right, titlefont=fonts)
 
+savefig(joinpath("figures", "measures_richness.png"))
