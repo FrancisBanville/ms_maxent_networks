a517a4d502b6d6923a19b5bc11693f0bedd2c956
Francis Banville <francis.banville@umontreal.ca>
2021-08-04 16:48:50 -0400

    :sparkles: predict food webs for all values of species richness

--- src/02_predict_networks.jl
+++ src/02_predict_networks.jl
@@ -75,3 +75,38 @@ predicted_networks_empL = UnipartiteNetwork.(predicted_matrices_empL)
 
 save(joinpath("data", "sim", "predicted_networks_empL.jld"), "data", predicted_networks_empL)
 
+## Predict the adjacency matrix for the entire range of worldwide species richness
+
+# Minimum and maximum number of species (mammals, birds, and amphibians) in BiodiversityMapping maps
+S_min = 5
+S_max = 950
+
+# Predict the adjacency matrix for this range of species richness (very long)
+S_map = S_min:S_max
+L_map = L_med[1:length(S_map)]
+
+predicted_matrices_map = []
+p = Progress(length(S_map))
+Threads.@threads for i in 1:length(S_map)
+    try
+       N = predict_adjacency_matrix(S_map[i], L_map[i], 50)
+       N = sparse(N)
+       push!(predicted_matrices_map, N)
+    catch
+      println("could not simulate network $(i)")
+    end
+    next!(p)
+end
+
+# 5 networks were not predicted properly (i = 557, 573, 579, 603, 637)
+i = vcat(557, 573, 579, 603, 637)
+predicted_matrices_2ndtry = predict_adjacency_matrix.(i.+4, L_med[i.+4], 50)
+
+for j in 1:length(i)
+  insert!(predicted_matrices_map, i[j], predicted_matrices_2ndtry[j])
+end
+
+predicted_networks_map = UnipartiteNetwork.(predicted_matrices_map)
+
+save(joinpath("data", "sim", "predicted_networks_map.jld"), "data", predicted_networks_map)
+
