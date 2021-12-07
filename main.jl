bcd2e134ea00de9d126d207c3a58b059ad1253cc
Francis Banville <francis.banville@umontreal.ca>
2021-12-07 17:13:09 -0500

    ✨ update main

--- main.jl
+++ main.jl
@@ -9,8 +9,9 @@ using DataFrames
 using DelimitedFiles
 using JLD
 
-import Ipopt
-using JuMP
+import Ipopt # solving a system of nonlinear equations numerically
+using JuMP # solving a system of nonlinear equations numerically
+
 using LinearAlgebra
 using Plots
 using ProgressMeter
@@ -25,9 +26,12 @@ using Mangal
 using RandomBooleanMatrices
 
 # Load custom functions
+include(joinpath("code", "functions", "predict_links.jl")) # predict the number of links from the number of species
+include(joinpath("code", "functions", "joint_degree_dist_maxent.jl")) # gives the joint degree distribution of maximum entropy given numbers of species and links
+include(joinpath("code", "functions", "degree_dist_maxent.jl")) # gives the degree distribution of maximum entropy given numbers of species and links
 
 # Load scripts
-include(joinpath("src", "01_import_mangal_metadata.jl"))
-include(joinpath("src", "02_predict_networks.jl"))
-include(joinpath("src", "03_make_figures.jl"))
+include(joinpath("code", "01_import_mangal_metadata.jl"))
+include(joinpath("code", "02_predict_networks.jl"))
+include(joinpath("code", "03_make_figures.jl"))
 
