4d2a4b9538f7dc57523667e10d82b1a90ac98e61
Francis Banville <francis.banville@umontreal.ca>
2021-12-07 14:21:26 -0500

    💥 remove spatial part of the code

--- main.jl
+++ main.jl
@@ -24,17 +24,10 @@ using EcologicalNetworks
 using Mangal
 using RandomBooleanMatrices
 
-using SimpleSDMLayers
-using ArchGDAL
-using GDAL
-
 # Load custom functions
-include(joinpath("src", "functions.jl")) 
 
 # Load scripts
 include(joinpath("src", "01_import_mangal_metadata.jl"))
 include(joinpath("src", "02_predict_networks.jl"))
 include(joinpath("src", "03_make_figures.jl"))
-include(joinpath("src", "04_make_maps.jl"))
-
 
