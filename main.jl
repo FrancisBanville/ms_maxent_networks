c544ed3dcc6129b47a9c7591c89da44bc387ec41
Francis Banville <francis.banville@umontreal.ca>
2021-12-09 17:51:38 -0500

    ✨ update main

--- main.jl
+++ main.jl
@@ -1,38 +1,47 @@
-# Activate project environment
+## Activate project environment
 import Pkg; Pkg.activate(".")
 
 Pkg.instantiate()
 
-# Load required packages
-import CSV
-using DataFrames
+## Load required packages
+
+# Manipulating variables, data frames and files
+import CSV 
+using DataFrames 
 using DelimitedFiles
-using JLD
+using JLD 
 
-import Ipopt # solving a system of nonlinear equations numerically
-using JuMP # solving a system of nonlinear equations numerically
+# Solving systems of equations numerically 
+import Ipopt
+using JuMP 
 
-using LinearAlgebra
-using Plots
+# Doing statistics and models
+using Distributions 
+using LinearAlgebra 
 using ProgressMeter
 using SparseArrays
 using Statistics
 using StatsBase
-using StatsPlots
 using Turing
 
-using EcologicalNetworks # analyzing ecological networks
-using Mangal # importing data on ecological networks
-using RandomBooleanMatrices
+# Making plots
+using Plots 
+using StatsPlots 
 
-# Load custom functions
+# Analyzing ecological networks
+using EcologicalNetworks 
+using Mangal 
+using RandomBooleanMatrices 
+
+## Load custom functions
 include(joinpath("code", "functions", "predict_links.jl")) # predict the number of links from the number of species
 include(joinpath("code", "functions", "joint_degree_dist_maxent.jl")) # gives the joint degree distribution of maximum entropy given numbers of species and links
 include(joinpath("code", "functions", "degree_dist_maxent.jl")) # gives the degree distribution of maximum entropy given numbers of species and links
 include(joinpath("code", "functions", "joint_degree_seq.jl")) # gives the joint degree sequence of a unipartite network
 include(joinpath("code", "functions", "joint_degree_dist.jl")) # gives the joint degree distribution of a unipartite network
+include(joinpath("code", "functions", "network_maxent.jl")) # gives the joint degree distribution of a unipartite network
 
-# Load scripts
+## Load scripts
 include(joinpath("code", "01_import_mangal_metadata.jl"))
 include(joinpath("code", "02_predict_networks.jl"))
 include(joinpath("code", "03_make_figures.jl"))
