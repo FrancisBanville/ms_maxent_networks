4677e93c32693097bbb4b417bdcf11feb5ddef8f
Francis Banville <francis.banville@umontreal.ca>
2021-12-09 17:52:46 -0500

    💥 remove old code

--- code/02_predict_networks.jl
+++ code/02_predict_networks.jl
@@ -1,6 +1,7 @@
 ## Read metadata of all food webs archived on mangal.io (generated from 01_import_mangal_metadata.jl)
-mangal_foodwebs = DataFrame(CSV.File(joinpath("data", "mangal_foodwebs.csv")))
-
+mangal_foodwebs = DataFrame(CSV.File(joinpath("data", "raw", "mangal_foodwebs.csv")))
+N = network.(mangal_foodwebs.id)
+N = convert.(UnipartiteNetwork, N)
 
 ## Define flexible links model and infer model parameters
 
@@ -56,57 +57,3 @@ Threads.@threads for s in sp
 end
 
 save(joinpath("data", "sim", "predicted_links.jld"), "data", predicted_links)
-
-# predicted number of links (median) for species richness in sp
-L_med = convert.(Int64, round.(median.(eachcol(predicted_links))))
-
-## Predict the adjacency matrix of maximum entropy for a range of species richness and median predicted number of links
-i = findall(in(S).(sp))
-Sp = sort(unique(S))
-
-predicted_matrices = predict_adjacency_matrix.(Sp, L_med[i], 500)
-predicted_networks = UnipartiteNetwork.(predicted_matrices)
-
-save(joinpath("data", "sim", "predicted_networks.jld"), "data", predicted_networks)
-
-## Predict the adjacency matrix of maximum entropy for networks with empirical numbers of links
-predicted_matrices_empL = predict_adjacency_matrix.(S, L, 500)
-predicted_networks_empL = UnipartiteNetwork.(predicted_matrices_empL)
-
-save(joinpath("data", "sim", "predicted_networks_empL.jld"), "data", predicted_networks_empL)
-
-## Predict the adjacency matrix for the entire range of worldwide species richness
-
-# Minimum and maximum number of species (mammals, birds, and amphibians) in BiodiversityMapping maps
-S_min = 5
-S_max = 950
-
-# Predict the adjacency matrix for this range of species richness (very long)
-S_map = S_min:S_max
-L_map = L_med[1:length(S_map)]
-
-predicted_matrices_map = []
-p = Progress(length(S_map))
-Threads.@threads for i in 1:length(S_map)
-    try
-       N = predict_adjacency_matrix(S_map[i], L_map[i], 50)
-       N = sparse(N)
-       push!(predicted_matrices_map, N)
-    catch
-      println("could not simulate network $(i)")
-    end
-    next!(p)
-end
-
-# 5 networks were not predicted properly (i = 557, 573, 579, 603, 637)
-i = vcat(557, 573, 579, 603, 637)
-predicted_matrices_2ndtry = predict_adjacency_matrix.(i.+4, L_med[i.+4], 50)
-
-for j in 1:length(i)
-  insert!(predicted_matrices_map, i[j], predicted_matrices_2ndtry[j])
-end
-
-predicted_networks_map = UnipartiteNetwork.(predicted_matrices_map)
-
-save(joinpath("data", "sim", "predicted_networks_map.jld"), "data", predicted_networks_map)
-
--- code/03_make_figures.jl
+++ code/03_make_figures.jl
@@ -53,88 +53,7 @@ savefig(joinpath("figures", "prop_disconnected_species.png"))
 
 
 
-
-
-
-
-
-
-
-
-
-################## OLD FIGURES ###############################
-
-## Figure: Divergence between numerical and analytical solutions of the MaxEnt degree distributions
-
-## Load metadata of all food webs archived on mangal.io (generated from 01_import_mangal_metadata.jl)
-mangal_foodwebs = DataFrame(CSV.File(joinpath("data", "mangal_foodwebs.csv")))
-
-# Load predicted links (generated from 02_predict_networks.jl)
-# Columns: S from 5 to 1000, Rows: 1000 simulations
-predicted_links = load(joinpath("data", "sim", "predicted_links.jld"))["data"]
-
-# Compute average degree for all values of species richness and median predicted numbers of links
-sp = collect(5:1000) 
-L_med = convert.(Int64, round.(median.(eachcol(predicted_links))))
-kavg = 2 .* L_med ./ sp
-
-"""
-estimate_lambda(S::Int64, kavg::Float64) 
-    S: number of species
-    kavg: mean degree constraint (2L/S)
-Returns the numerical value of the Lagrange multiplier 
-"""
-function estimate_lambda(S::Int64, kavg::Float64) 
-  model = Model(Ipopt.Optimizer)
-  set_silent(model)
-
-  @variable(model, x >= 0)  # lambdas (x) are positive real numbers 
-  @NLobjective(model, Max, x) # could have been another objective function
-  @NLconstraint(model, sum(k * exp(-x * k) for k = 1:S) / sum(exp(-x * k) for k = 1:S) == kavg)
-  optimize!(model)
-  return value.(x)
-end
-
-"""
-compute_divergence(S::Int64, kavg::Float64)
-    S: number of species
-    kavg: mean degree constraint (2L/S)
-Returns the divergence (sum of absolute differences) between the numerical and analytical solutions of the MaxEnt degree distributions
-"""
-function compute_divergence(S::Int64, kavg::Float64)
-  lambda = estimate_lambda(S, kavg)
-  pk_numerical = [exp(-lambda * k) for k in 1:S] ./ sum([exp(-lambda * k) for k = 1:S])
-
-  c = 1 / (kavg - 1)
-  r = (kavg - 1) / kavg
-  pk_analytical = (c * r^k for k in 1:S)
-
-  divergence = sum(abs.(pk_numerical .- pk_analytical))
-  return divergence
-end
-
-# Compute divergence for all values of species richness
-divergence = compute_divergence.(sp, kavg) 
-
-# Compute quantiles of species richness for all networks archived on Mangal (to be plotted)
-species500 = quantile(mangal_foodwebs.S, 0.5)
-species015 = quantile(mangal_foodwebs.S, 0.015)
-species985 = quantile(mangal_foodwebs.S, 0.985)
-
-# Plot divergence between numerical and analytical solutions of the MaxEnt degree distributions
-scatter(sp[1:150], divergence[1:150], alpha=0.4, lab="", 
-  framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
-  guidefont=fonts, xtickfont=fonts, ytickfont=fonts)
-plot!([species500], seriestype=:vline, color=:grey, ls=:dash, lab="", ylim=(0,0.12))
-plot!([species015, species985], [0.12, 0.12], fill=(0, :grey, 0.12), c=:transparent, lab="")
-plot!([species015], seriestype=:vline, color=:grey, ls=:dot, lab="")
-plot!([species985], seriestype=:vline, color=:grey, ls=:dot, lab="")
-xaxis!(xlabel="Species richness")
-yaxis!(ylabel="Divergence")
-
-savefig(joinpath("figures", "divergence_numerical_analytical.png"))
-
-
+################# TK TO DO #########################
 ## Figure: Mean degree constraints and MaxEnt degree distributions
 species015 = Int64(round(species015))
 species500 = Int64(round(species500))
@@ -208,148 +127,10 @@ plot(plotA, plotB,
     titleloc=:right, titlefont=fonts)
 
 savefig(joinpath("figures","maxent_degree_distributions"))
+###########################################################
 
-
-## Figure: MaxEnt and empirical degree distributions
-
-# Get Mangal food webs with given numbers of species (median and 97% PI)
-species985 = 81 # no network with 85 species (but some with 81 and 89 species)
-
-Ns_mangal500 = mangal_foodwebs[mangal_foodwebs.S .== species500, :]
-Ns_mangal015 = mangal_foodwebs[mangal_foodwebs.S .== species015, :]
-Ns_mangal985 = mangal_foodwebs[mangal_foodwebs.S .== species985, :] 
-
-# Get food webs with minimum and maximum numbers of links (from food webs with median and 97% PI of species richness) (arbitrary criteria to select networks that will be plotted)
-N_mangal500min = Ns_mangal500[Ns_mangal500.L .== minimum(Ns_mangal500.L), :][1,:]
-N_mangal500max = Ns_mangal500[Ns_mangal500.L .== maximum(Ns_mangal500.L), :][1,:]
-N_mangal015min = Ns_mangal015[Ns_mangal015.L .== minimum(Ns_mangal015.L), :][2,:] # 2 food webs with 7 species and 8 links (get the one with predatory links)
-N_mangal015max = Ns_mangal015[Ns_mangal015.L .== maximum(Ns_mangal015.L), :][1,:]
-N_mangal985min = Ns_mangal985[Ns_mangal985.L .== minimum(Ns_mangal985.L), :][1,:]
-N_mangal985max = Ns_mangal985[Ns_mangal985.L .== maximum(Ns_mangal985.L), :][1,:]
-
-"""
-get_ranked_dd(N::DataFrameRow)
-    N: Data frame row of Mangal metadata
-Returns the ranked degree distribution of the input network
-"""
-function get_ranked_dd(N::DataFrameRow)
-    # Convert to UnipartiteNetwork
-    N = convert(UnipartiteNetwork, network(N.id))
-    # Get sorted degree distribution 
-    dd = sort(collect(values(degree(N))), rev=true)
-    return dd
-end
-
-# Get degree distribution for all selcted food webs
-dd_mangal500min = get_ranked_dd(N_mangal500min)
-dd_mangal500max = get_ranked_dd(N_mangal500max)
-dd_mangal015min = get_ranked_dd(N_mangal015min)
-dd_mangal015max = get_ranked_dd(N_mangal015max)
-dd_mangal985min = get_ranked_dd(N_mangal985min)
-
-
-# Get simulated networks with simulated numbers of links (median of flexible links model)
-Ns_maxent_fl = load(joinpath("data", "sim", "predicted_networks.jld"))["data"]
-
-# Get simulated networks with same numbers of species as the food webs previously selected
-S_maxent_fl = richness.(Ns_maxent_fl)
-
-N_maxent_fl500 = Ns_maxent_fl[S_maxent_fl .== species500][1]
-N_maxent_fl015 = Ns_maxent_fl[S_maxent_fl .== species015][1]
-N_maxent_fl985 = Ns_maxent_fl[S_maxent_fl .== species985][1]
-
-"""
-get_ranked_dd(N::UnipartiteNetwork)
-    N: Unipartite Network
-Returns the ranked degree distribution of the input network
-"""
-function get_ranked_dd(N::UnipartiteNetwork)
-  # Get sorted degree distribution 
-  dd = sort(collect(values(degree(N))), rev=true)
-  return dd
-end
-
-# Get degree distributions of all selected simulated networks
-dd_maxent_fl500 = get_ranked_dd(N_maxent_fl500)
-dd_maxent_fl015 = get_ranked_dd(N_maxent_fl015)
-dd_maxent_fl985 = get_ranked_dd(N_maxent_fl985)
-
-
-# Get simulated networks with the same numbers of links as food webs on Mangal
-Ns_maxent_empL = load(joinpath("data", "sim", "predicted_networks_empL.jld"))["data"]
-
-"""
-get_network_empL(S::Int64, fun::String) 
-    S: Species richness
-    fun: Either "min" or "max"
-Returns the simulated network of S species with the lowest (fun = "min") or higest (fun = "max") numbers of links
-"""
-function get_network_empL(S::Int64, fun::String) 
-  # Get simulated networks with S species
-  Ns_S = Ns_maxent_empL[richness.(Ns_maxent_empL) .== S]
-  
-  # Get the network with the lowest (min) or highest (max) numbers of links
-  if fun == "min"
-    N = Ns_S[links.(Ns_S) .== minimum(links.(Ns_S))][1]
-  elseif fun == "max"
-    N = Ns_S[links.(Ns_S) .== maximum(links.(Ns_S))][1]
-  else 
-    message("The function must be min or max")
-  end
-  return N
-end
-
-# Get simulated networks with same numbers of species and links as previously selected food webs
-N_maxent_empL500min = get_network_empL(species500, "min")
-N_maxent_empL500max = get_network_empL(species500, "max")
-N_maxent_empL015min = get_network_empL(species015, "min")
-N_maxent_empL015max = get_network_empL(species015, "max")
-N_maxent_empL985min = get_network_empL(species985, "min")
-
-# Get degree distributions of all selected simulated networks
-dd_maxent_empL500min = get_ranked_dd(N_maxent_empL500min)
-dd_maxent_empL500max = get_ranked_dd(N_maxent_empL500max)
-dd_maxent_empL015min = get_ranked_dd(N_maxent_empL015min)
-dd_maxent_empL015max = get_ranked_dd(N_maxent_empL015max)
-dd_maxent_empL985min = get_ranked_dd(N_maxent_empL985min)
-
-
-"""
-plot_dd(mangalmin::T, mangalmax::T, empLmin::T, empLmax::T, fl::T) where {T <: Vector{Int64}}
-    mangalmin: Degree distribution of Mangal food web of S species with lowest number of links
-    mangalmax: Degree distribution of Mangal food web of S species and highest number of links
-    empLmin: Degree distribution of simulated food web of S species and lowest number of links (same number of links as Mangal food webs)
-    empLmax: Degree distribution of simulated food web of S species and highest number of links (same number of links as Mangal food webs)
-    fl: Degree distribution of simulated food web of S and number of links given by the median of the flexible links model
-Returns a plot of the degree distributions of Mangal and empirical food webs with S species
-"""
-function plot_dd(mangalmin::T, mangalmax::T, empLmin::T, empLmax::T, fl::T) where {T <: Vector{Int64}}
-    plot(mangalmin, color=:black, alpha=0.7, linewidth=2, linestyle=:dot, label="Mangal", 
-      framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
-      guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
-      foreground_color_legend=nothing, background_color_legend=:white, legendfont=fonts,
-      xlab="Rank", ylab="Degree k")
-  plot!(mangalmax, color=:darkblue, alpha=0.7, linewidth=2, linestyle=:dot, label="")
-  plot!(empLmin, color=:black, alpha=0.7, linewidth=2, linestyle=:solid, label="MaxEnt")
-  plot!(empLmax, color=:darkblue, alpha=0.7, linewidth=2, linestyle=:solid, label="")
-  plot!(fl, color=:darkred, alpha=0.7, linewidth=2, linestyle=:solid, label="")
-end
-
-# Plot the degree distributions of Mangal and empirical food webs with median and 97% PI numbers of species
-plotA = plot_dd(dd_mangal015min, dd_mangal015max, dd_maxent_empL015min, dd_maxent_empL015max, dd_maxent_fl015)
-plotB = plot_dd(dd_mangal500min, dd_mangal500max, dd_maxent_empL500min, dd_maxent_empL500max, dd_maxent_fl500)
-plotC = plot_dd(dd_mangal985min, dd_mangal985min, dd_maxent_empL985min, dd_maxent_empL985min, dd_maxent_fl985)
-
-plot(plotA, plotB, plotC, 
-    layout=(1,3),
-    title = ["(a) S=$(species015) species" "(b) S=$(species500) species" "(c) S=$(species985) species"],
-    titleloc=:right, titlefont=fonts)
-
-savefig(joinpath("figures","mangal_maxent_degree_distributions"))
-
-
-
-## Figures: Measures of empirical and maximum entropy food webs 
+################# TK TO DO #########################
+# Figure: Measures of empirical and maximum entropy food webs 
 
 # Convert food webs archieved on Mangal to UnipartiteNetworks
 Ns_mangal = network.(mangal_foodwebs.id)
@@ -384,20 +165,6 @@ diameter_maxent_empL = maximum.(shortest_paths_maxent_empL)
 
 shortest_paths_maxent_fl = shortest_path.(Ns_maxent_fl)
 diameter_maxent_fl = maximum.(shortest_paths_maxent_fl)
-
-# Measure SVD-entropy
-"""
-svd_entropy(N::T) where {T <: UnipartiteNetwork}
-    N: unipartite network
-Returns the entropy of the adjacency matrix of a deterministic network using the singular values from a Singualar Value Decomposition
-"""
-function svd_entropy(N::T) where {T <: UnipartiteNetwork}
-    A = convert(Matrix, N.edges)
-    F = svd(A)
-    Λ = F.S[1:rank(A)]
-    λ = Λ ./ sum(Λ)
-    return -sum(λ .* log.(λ)) * 1 / log(length(λ))
-end
 
 entropy_mangal = svd_entropy.(Ns_mangal)
 entropy_maxent_empL = svd_entropy.(Ns_maxent_empL)
@@ -463,9 +230,10 @@ plot(plotA, plotB, plotC, plotD,
      titleloc=:right, titlefont=fonts)
 
 savefig(joinpath("figures", "measures_mangal_maxent.png"))
+#########################################################
 
-
-# Plot measures as a function of species richness
+################# TK TO DO #########################
+# Figure: Plot measures as a function of species richness and of other measures
 a = [5,10,20,50,100,400] # specified x-ticks 
 
 plotA = scatter(S_mangal, nestedness_maxent_empL, alpha=0.3, label="Empirical L", smooth=true, markersize=3, linestyle=:dot, linealpha=1,
@@ -533,10 +301,11 @@ scatter!(tl_max_maxent_fl, nestedness_maxent_fl, alpha=0.5, label="Median L", sm
 scatter!(tl_max_mangal, nestedness_mangal, alpha=0.5, label="Mangal", smooth=true, markersize=4, linestyle=:dot, linealpha=1)
 
 savefig(joinpath("figures", "maxtrophiclevel_nestedness.png"))
+#############################################################
 
 
+################# TK TO DO #########################
 ## Figure: Entropy and divergence of degree distributions
-
 """
 get_divergence_dd(N1::UnipartiteNetwork{Bool, MangalNode}, N2::UnipartiteNetwork{Bool, String})  
     N1: Empirical food web
@@ -592,8 +361,9 @@ plot(plotA, plotB,
      titleloc=:right, titlefont=fonts)
      
 savefig(joinpath("figures", "divergence_degree_distributions.png"))
+################################################################################
 
-
+################# TK TO DO #########################
 ## Figure: Distribution of entropy and z-scores of empirical food webs
 
 plotA = density(entropy_maxent_empL, label="Empirical L",
@@ -631,75 +401,4 @@ plot(plotA, plotB,
      titleloc=:right, titlefont=fonts)
 
 savefig(joinpath("figures", "entropy_distribution.png"))
-
-
-## Figure: Motif distribution
-
-"""
-count_motifs(N::T) where {T <: UnipartiteNetwork}
-    N: unipartite network
-Returns the number of times each motif was found in the unipartite network
-"""
-function count_motifs(N::T) where {T <: UnipartiteNetwork}
-  ms = unipartitemotifs() # list motifs
-  ms_count = [length(find_motif(N, ms[i])) for i in 1:13]
-end
-
-"""
-count_motifs(Ns::T) where {T <: Union{Vector{UnipartiteNetwork{Bool, String}}, Vector{UnipartiteNetwork{Bool, MangalNode}}}}
-    Ns: vector of unipartite networks
-Returns the proportion of each motif in each unipartite network
-Rows are networks and columns are different motifs
-"""
-function count_motifs(Ns::T) where {T <: Union{Vector{UnipartiteNetwork{Bool, String}}, Vector{UnipartiteNetwork{Bool, MangalNode}}}}
-  # remove the biggest network (too long to find all motifs in networks that are too large)
-  Ns_small = Ns[richness.(Ns) .!== maximum(richness.(Ns))]
-  # count each of the 13 possible motifs for all networks in Ns
-  motifs_count = zeros(Float64, length(Ns_small), length(unipartitemotifs()))
-  
-  for i in 1:length(Ns_small)
-    motifs_count[i,:] = count_motifs(Ns_small[i]) 
-  end
-
-  # returns the proportion of each motif for all networks
-  return motifs_count ./ sum.(eachrow(motifs_count))
-end
-
-# Get the proportion of motifs for all food webs (empirical and predicted)
-motifs_prop_mangal = count_motifs(Ns_mangal)
-motifs_prop_maxent_empL = count_motifs(Ns_maxent_empL)
-motifs_prop_maxent_fl = count_motifs(Ns_maxent_fl)
-
-"""
-plot_motifs(ms::T) where {T <: Matrix{Float64}}
-    ms: matrix of proportions of motifs in networks (rows = networks, columns = motifs)
-Returns the plot of the distribution of all motifs in a set of food webs 
-Gives a violon plot, boxplot and dotplot
-"""
-function plot_motifs(ms::T) where {T <: Matrix{Float64}}
-  
-  # change data format for plotting
-  ms_df = convert(DataFrame, ms)
-  rename!(ms_df, vcat(["S$i" for i in 1:5],["D$j" for j in 1:8]))
-  ms_df = DataFrames.stack(ms_df)
-  
-  # plot proportions of all modules
-  @df ms_df violin(string.(:variable), :value, linewidth=0, label="",
-              framestyle=:box, dpi=1000, size=(800,500), margin=5Plots.mm, 
-              guidefont=fonts, xtickfont=fonts, ytickfont=fonts,
-              ylims=(0,1),
-              xaxis="Motifs", yaxis="Proportion")
-  @df ms_df boxplot!(string.(:variable), :value, fillalpha=0.2, markersize=3, linewidth=1, color=:grey, label="")
-  @df ms_df dotplot!(string.(:variable), :value, color=:black, markersize=1.5, alpha=0.15, label="")
-end
-
-# Plot the proportions of all motifs for empirical and predicted food webs 
-# Food webs with median L are not plotted since their distribution is very similar to the one of MaxEnt food webs with empirical L
-plotA = plot_motifs(motifs_prop_mangal)
-plotB = plot_motifs(motifs_prop_maxent_empL)
-
-plot(plotA, plotB,
-     title = ["(a) Empirical food webs" "(b) MaxEnt food webs"],
-     titleloc=:right, titlefont=fonts)
-
-savefig(joinpath("figures", "motifs_distribution.png"))
+#####################################################################
