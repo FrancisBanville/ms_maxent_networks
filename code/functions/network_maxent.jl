26a78c23e9dea91ba325eb8550d2086ff5e150fc
Francis Banville <francis.banville@umontreal.ca>
2022-01-17 13:55:56 -0500

    🐛 fix bug in the simulating algorithm

--- code/functions/network_maxent.jl
+++ code/functions/network_maxent.jl
@@ -15,16 +15,18 @@ end
 network_maxent(N::UnipartiteNetwork, nsteps::Int64, T::Float64)
     N: Unipartite directed simple network
     nsteps: Number of steps
-    T: Temperature of the simulating annealing algorithm
 Returns the adjacency matrix of maximum SVD-entropy constrained by the joint degree sequence of N using a simulating annealing algorithm 
 """
-function network_maxent(N::UnipartiteNetwork, nsteps::Int64, T::Float64)
+function network_maxent(N::UnipartiteNetwork, nsteps::Int64)
     A = convert(Matrix, N.edges) # convert UnipartiteNetwork to dense matrix format
     rmg = matrixrandomizer(A) # matrix generator object
 
     # initial random matrix that has the same joint degree sequence as N
     A0 = convert(Matrix{Bool}, rand(rmg))
     
+    # initial temperature of the simulating annealing algorithm
+    T = exp(-10/nsteps)  
+
     # simulating annealing algorithm
     for i in 1:nsteps
         # propose a new constrained random matrix and compute the difference in SVD-entropy 
@@ -42,9 +44,10 @@ function network_maxent(N::UnipartiteNetwork, nsteps::Int64, T::Float64)
         end
 
         # update the temperature
-        T <- T * exp(-100/nsim)  
+        T = T * exp(-10/nsteps)  
     end
     max_entropy = svd_entropy(A0)
     Amax = (A = A0, entropy = max_entropy)
     return Amax
 end
+
